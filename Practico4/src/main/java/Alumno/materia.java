4b821d38f1632a1352a1c4d9644e45602c5dcbb2
Lucio Ponce <luciodanielps@gmail.com>
2025-09-03 18:59:28 -0300

    Update materia

--- Practico4/src/main/java/Alumno/materia.java
+++ Practico4/src/main/java/Alumno/materia.java
@@ -1,7 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package Alumno;
 
 /**
@@ -9,5 +6,51 @@ package Alumno;
  * @author lucio
  */
 public class materia {
+    
+    public int idMateria;
+    public String nombre;
+    public int anio;
+
+    public materia(int idMateria, String nombre, int anio) {
+        
+        this.idMateria = idMateria;
+        this.nombre = nombre;
+        this.anio = anio;
+    }
+
+    public int getIdMateria() {
+        
+        return idMateria;
+    }
+
+    public void setIdMateria(int idMateria) {
+        
+        this.idMateria = idMateria;
+    }
+
+    public String getNombre() {
+        
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        
+        this.nombre = nombre;
+    }
+
+    public int getAnio() {
+        
+        return anio;
+    }
+
+    public void setAnio(int anio) {
+        
+        this.anio = anio;
+    }
+    
+    
+    
+    
+    
     
 }
