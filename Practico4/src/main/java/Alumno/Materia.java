521b1199834fbe9e7c1a71ff6ff6bb4f358a4978
Lucio Ponce <luciodanielps@gmail.com>
2025-09-03 20:20:22 -0300

    Update Materia

--- Practico4/src/main/java/Alumno/Materia.java
+++ Practico4/src/main/java/Alumno/Materia.java
@@ -7,7 +7,46 @@ package Alumno;
 /**
  *
  * @author lucio
- */
-public class Materia {
+ */public class Materia {
     
-}
+    public int idMateria;
+    public String nombre;
+    public int anio;
+
+    public Materia(int idMateria, String nombre, int anio) {
+        
+        this.idMateria = idMateria;
+        this.nombre = nombre;
+        this.anio = anio;
+    }
+
+    public int getIdMateria() {
+        
+        return idMateria;
+    }
+
+    public void setIdMateria(int idMateria) {
+        
+        this.idMateria = idMateria;
+    }
+
+    public String getNombre() {
+        
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        
+        this.nombre = nombre;
+    }
+
+    public int getAnio() {
+        
+        return anio;
+    }
+
+    public void setAnio(int anio) {
+        
+        this.anio = anio;
+    }
+ }
